0b829b3d59983cd0566dd9b612fa887f0185e24a
💗 쓸💗 <sseul@ui-MacBookAir.local>
2022-07-06 13:31:34 +0900

    Add: List Removing

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const DiaryItem = ({
   onRemove,
   author,
@@ -6,6 +8,18 @@ const DiaryItem = ({
   emotion,
   id,
 }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+
+  //textarea의 input을 핸들링할 state
+  const [localContent, setLocalContent] = useState('');
+
+  const handleRemove = () => {
+    if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+      onRemove(id);
+    }
+  };
+
   return (
     <div className='DiaryItem'>
       <div className='info'>
@@ -15,16 +29,30 @@ const DiaryItem = ({
         <br />
         <span className='date'>{new Date(created_date).toLocaleString()}</span>
       </div>
-      <div className='content'>{content}</div>
-      <button
-        onClick={() => {
-          if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
-            onRemove(id);
-          }
-        }}
-      >
-        삭제하기
-      </button>
+      <div className='content'>
+        {isEdit ? (
+          <>
+            <textarea
+              value={localContent}
+              onChange={(e) => setLocalContent(e.target.value)}
+            />
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </div>
+
+      {isEdit ? (
+        <>
+          <button onClick={toggleIsEdit}>수정 취소</button>
+          <button>수정 완료</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleRemove}>삭제하기</button>
+          <button onClick={toggleIsEdit}>수정하기</button>
+        </>
+      )}
     </div>
   );
 };
